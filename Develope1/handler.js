54f88951d6415e6dcedd03aac6ab2a375c126623
sounakchatterjee00 <sounakchatterjee00@gmail.com>
2020-07-05 15:49:27 +0530

    dev1 cchng2

--- Develope1/handler.js
+++ Develope1/handler.js
@@ -5,7 +5,7 @@ module.exports.hello = async event => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Forced change',
+        message: 'Forced change1',
         input: event,
       },
       null,
